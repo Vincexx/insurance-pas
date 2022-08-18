b1da2eb4c280a93fcb2f452472152d275a07e4c5
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-08-18 17:36:42 +0800

    ddynamic error message

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaExclamationCircle } from "react-icons/fa";
 
-const ErrorMessage = () => {
+const ErrorMessage = ({ message }) => {
   return (
     <div className="bg-red-500 flex justify-between items-center text-white p-2 rounded-md">
-      <p className="text-sm">Invalid Credentials</p>
+      <p className="text-sm">{message}</p>
       <FaExclamationCircle />
     </div>
   );
