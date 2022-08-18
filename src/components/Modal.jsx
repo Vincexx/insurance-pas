f036f45fc575802df4b797c0e69f9642dea43647
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-08-18 18:02:51 +0800

    added framer motion

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { FaCheck } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Modal = ({ icon, status, message }) => {
   return (
     <div className="bg-zinc-200 opacity-90 fixed inset-0 z-50">
       <div className="flex justify-center items-center h-screen">
-        <div className="bg-white rounded-xl py-12 px-24 flex flex-col justify-center items-center">
+        <motion.div
+          initial={{ y: "-1000px" }}
+          animate={{ y: "-250px" }}
+          transition={{ type: "spring", duration: 0.5 }}
+          className="bg-white rounded-xl py-6 px-24 flex flex-col justify-center items-center"
+        >
           {icon}
           <p className="font-bold text-3xl text-gray-900">{status}</p>
           <p className="text-gray-700 mt-3 text-md">{message}</p>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
