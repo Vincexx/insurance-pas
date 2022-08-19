3d34af125aba98d8c9c5f5e866e7d3a72c11eb42
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-08-19 17:55:59 +0800

    added fetch all users

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaMagnet, FaWindowClose, FaHamburger } from "react-icons/fa";
+import { FaMagnet, FaWindowClose, FaHamburger, FaCheck } from "react-icons/fa";
 import Button from "./Button";
 import { useEffect } from "react";
 import { useAuth } from "../App";
+import ErrorMessage from "./ErrorMessage";
+import Modal from "./AlertModal";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,63 +14,79 @@ const Header = () => {
   useEffect(() => {}, [token]);
 
   const [open, setOpen] = useState(false);
+  const [loggedOut, setLoggedOut] = useState(false);
 
   const logout = () => {
     localStorage.removeItem("token");
-    navigate("/login");
+    setLoggedOut(true);
+    setTimeout(() => {
+      setLoggedOut(false);
+      navigate("/login");
+    }, 2000);
   };
 
   const publicRoutes = [{ name: "Dashboard", link: "/dashboard" }];
   const privateRoutes = [
     { name: "Login", link: "/login" },
-    { name: "Register", link: "/register" },
+    { name: "Register",
+    
+     link: "/register" },
   ];
   const navs = !token ? privateRoutes : publicRoutes;
 
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
-        <Link to={"/"}>
-          <div className="flex items-center font-bold cursor-pointer text-xl">
-            <span className="mr-1 text-green-500">
-              <FaMagnet />
-            </span>
-            <p>Majesco</p>
+    <>
+      {loggedOut && (
+        <Modal
+          icon={<FaCheck className="text-green-500 text-4xl" />}
+          status={"Success"}
+          message={"Logged out"}
+        />
+      )}
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
+          <Link to={"/"}>
+            <div className="flex items-center font-bold cursor-pointer text-xl">
+              <span className="mr-1 text-green-500">
+                <FaMagnet />
+              </span>
+              <p>Majesco</p>
+            </div>
+          </Link>
+
+          <div className="cursor-pointer" onClick={() => setOpen(!open)}>
+            {open ? (
+              <FaWindowClose className="text-2xl text-green-500 absolute right-8 top-5 md:hidden" />
+            ) : (
+              <FaHamburger className="text-2xl text-green-500 absolute right-8 top-5 md:hidden" />
+            )}
           </div>
-        </Link>
 
-        <div className="cursor-pointer" onClick={() => setOpen(!open)}>
-          {open ? (
-            <FaWindowClose className="text-2xl text-green-500 absolute right-8 top-5 md:hidden" />
-          ) : (
-            <FaHamburger className="text-2xl text-green-500 absolute right-8 top-5 md:hidden" />
-          )}
-        </div>
-
-        <ul
-          className={`shadow-md md:shadow-none md:flex md:items-center absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 md:pb-0 pb-7 transition-all duration-500 ease-in
+          <ul
+            className={`shadow-md md:shadow-none md:flex md:items-center absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 md:pb-0 pb-7 transition-all duration-500 ease-in
           ${
             open
               ? "top-20 opacity-100"
               : "top-[-490px] md:opacity-100 opacity-0"
           }`}
-        >
-          {navs.map((item, id) => (
-            <li
-              className="my-7 md:my-0 md:ml-8 text-md text-gray-600 hover:text-green-500 transition-all duration-100 font-thin"
-              key={id}
-            >
-              <Link to={item.link}>{item.name}</Link>
-            </li>
-          ))}
-          {!token ? (
-            <Button btnName={"Get Started"} />
-          ) : (
-            <Button btnName={"Logout"} action={() => logout()} />
-          )}
-        </ul>
+          >
+            {navs.map((item, id) => (
+              <li
+                className="my-7 md:my-0 md:ml-8 text-md text-gray-600 hover:text-green-500 transition-all duration-100 font-thin"
+                key={id}
+              >
+                <Link to={item.link}>{item.name}</Link>
+              </li>
+            ))}
+            {!token ? (
+              <Button btnName={"Get Started"} />
+            ) : (
+              <Button btnName={"Logout"} action={() => logout()} />
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,20 +1,33 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Button from "../components/Button";
 
 const Account = () => {
-  const [users, setUsers] = useState([
-    { id: 1, name: "Charles", age: 12 },
-    { id: 2, name: "Charles", age: 12 },
-    { id: 3, name: "Charles", age: 12 },
-  ]);
-  const [name, setName] = useState("");
+  const header = [
+    "Account #",
+    "Firstname",
+    "Middlename",
+    "Lastname",
+    "Email",
+    "Address",
+    "Edit",
+    "Delete",
+  ];
+  const [users, setUsers] = useState([]);
 
-  const handleSubmit = () => {
-    const isExist = users.find((item) => item.name === name);
-
-    if (isExist) console.log("Account already exist");
-    else console.log("Account has been added");
+  const fetchAccounts = async () => {
+    await axios
+      .get(`${process.env.REACT_APP_API_URL}/api/admin/users`)
+      .then((res) => {
+        console.log(res.data);
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
   };
+
+  useEffect(() => {
+    fetchAccounts();
+  }, []);
 
   return (
     <>
@@ -30,111 +43,56 @@ const Account = () => {
                 <table className="min-w-full divide-y divide-gray-200">
                   <thead className="bg-gray-50">
                     <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                      >
-                        ID
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                      >
-                        Email
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
-                      >
-                        Edit
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
-                      >
-                        Delete
-                      </th>
+                      {header.map((item, index) => (
+                        <th
+                          key={index}
+                          scope="col"
+                          className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                        >
+                          {item}
+                        </th>
+                      ))}
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200">
-                    <tr>
-                      <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                        1
-                      </td>
-                      <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                        Jone Doe
-                      </td>
-                      <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                        jonne62@gmail.com
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a
-                          className="text-green-500 hover:text-green-700"
-                          href="#"
-                        >
-                          Edit
-                        </a>
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a className="text-red-500 hover:text-red-700" href="#">
-                          Delete
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                        2
-                      </td>
-                      <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                        Jone Doe
-                      </td>
-                      <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                        jonne62@gmail.com
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a
-                          className="text-green-500 hover:text-green-700"
-                          href="#"
-                        >
-                          Edit
-                        </a>
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a className="text-red-500 hover:text-red-700" href="#">
-                          Delete
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                        3
-                      </td>
-                      <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                        Jone Doe
-                      </td>
-                      <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                        jonne62@gmail.com
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a
-                          className="text-green-500 hover:text-green-700"
-                          href="#"
-                        >
-                          Edit
-                        </a>
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a className="text-red-500 hover:text-red-700" href="#">
-                          Delete
-                        </a>
-                      </td>
-                    </tr>
+                    {users.map((item, index) => (
+                      <tr key={index}>
+                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {item.accountNumber ? item.accountNumber : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                          {item.firstName ? item.firstName : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                          {item.middleName ? item.middleName : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                          {item.lastName ? item.lastName : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                          {item.email ? item.email : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                          {item.address ? item.address : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                          <a
+                            className="text-green-500 hover:text-green-700"
+                            href="#"
+                          >
+                            Edit
+                          </a>
+                        </td>
+                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                          <a
+                            className="text-red-500 hover:text-red-700"
+                            href="#"
+                          >
+                            Delete
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
