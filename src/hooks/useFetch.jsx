6a19d958d8b3f7691170ae68ceda55bf8e76db4a
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-08-25 11:55:14 +0800

    optimized code

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import { useAuth } from "../App";
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
