67418d7edcd4cca7fcb9e8fff08c4bfdfcd42040
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-08-31 13:24:14 +0800

    update desc

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -8,10 +8,10 @@ const Index = () => {
           Welcome to Automobile Insurance Policy and Claims System
         </h1>
         <p className="text-left my-3 text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam harum
-          consequuntur nihil rem modi tempora iusto, quisquam praesentium dolore
-          voluptatum itaque nisi totam pariatur voluptates earum officiis amet
-          reprehenderit ea.
+          In this application, a simple Automobile Insurance Policy and
+          Claims Administration system (PAS) will be created to manage customer
+          automobile insurance policies and as well as accident claims for an
+          insurance company.
         </p>
         <button className="bg-blue-500 font-bold text-white py-2 px-3 rounded">
           Learn More
